d2a73af8df50b3533037ea30804ae29d10e8afe9
Shivam Vijaywargiya <shivam.vijaywargiya@soroco.com>
2018-02-05 22:45:24 +0530

    working2

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
     args = parser.parse_args()
     port = args.port
-
-    app.run(host='127.0.0.1', port=10001)
+    app.run(debug=True, use_reloader=True)
+    #app.run(host='127.0.0.1', port=10001)
 
 
