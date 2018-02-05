bec113c0ba733be1c8d0a0e00bc4cf5f4b262831
Shivam Vijaywargiya <shivam.vijaywargiya@soroco.com>
2018-02-05 22:49:39 +0530

    w3

--- blockchain/routes.py
+++ blockchain/routes.py
@@ -12,6 +12,15 @@ blockchain = Blockchain()
 
 app = Flask(__name__)
 
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
 
 @app.route('/mine', methods=['GET'])
 def mine():
