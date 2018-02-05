fd6c70fe0dbb2997d622ba587458463661161173
Shivam Vijaywargiya <shivam.vijaywargiya@soroco.com>
2018-02-05 22:54:49 +0530

    w4

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from blockchain.routes import app
 
 
 if __name__ == '__main__':
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
-    args = parser.parse_args()
-    port = args.port
+    #from argparse import ArgumentParser
+#
+    #parser = ArgumentParser()
+    #parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    #args = parser.parse_args()
+    #port = args.port
     app.run(debug=True, use_reloader=True)
     #app.run(host='127.0.0.1', port=10001)
 
